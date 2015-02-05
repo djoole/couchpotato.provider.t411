0ea7fce22b96f5ef6b1e2aa6bc126c48dc77a7ce
djoole <bobby.djoole@gmail.com>
2015-02-05 21:43:33 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,62 @@
+from .main import t411
+
+def autoload():
+    return t411()
+
+config = [{
+    'name': 't411',
+    'groups': [
+        {
+            'tab': 'searcher',
+            'list': 'torrent_providers',
+            'name': 't411',
+            'description': 'See <a href="https://www.t411.me/">T411</a>',
+            'wizard': True,
+            'options': [
+                {
+                    'name': 'enabled',
+                    'type': 'enabler',
+                    'default': False,
+                },
+                {
+                    'name': 'username',
+                    'default': '',
+                },
+                {
+                    'name': 'password',
+                    'default': '',
+                    'type': 'password',
+                },
+                {
+                    'name': 'ignore_year',
+                    'label': 'ignore year',
+                    'default': 0,
+                    'type': 'bool',
+                    'description': 'Will ignore the year in the search results',
+                },
+                {
+                    'name': 'seed_ratio',
+                    'label': 'Seed ratio',
+                    'type': 'float',
+                    'default': 1,
+                    'description': 'Will not be (re)moved until this seed ratio is met.',
+                },
+                {
+                    'name': 'seed_time',
+                    'label': 'Seed time',
+                    'type': 'int',
+                    'default': 40,
+                    'description': 'Will not be (re)moved until this seed time (in hours) is met.',
+                },
+                {
+                    'name': 'extra_score',
+                    'advanced': True,
+                    'label': 'Extra Score',
+                    'type': 'int',
+                    'default': 20,
+                    'description': 'Starting score for each release found via this provider.',
+                }
+            ],
+        },
+    ],
+}]
