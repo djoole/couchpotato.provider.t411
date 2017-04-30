59cfa5a5518945a2243022abf6efb043ad444f8a
ahyear <kevindelouya@gmail.com>
2017-05-01 00:21:28 +0200

    update link url to .ai

--- t411/__init__.py
+++ t411/__init__.py
@@ -10,7 +10,7 @@ config = [{
             'tab': 'searcher',
             'list': 'torrent_providers',
             'name': 't411',
-            'description': 'See <a href="https://www.t411.ch/">T411</a>',
+            'description': 'See <a href="https://www.t411.ai/">T411</a>',
             'icon': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAc5JREFUOI3tkk9IU3EAxz/vrbWeb+/NNdtmBS5kEHWwYfYHKulWCbtIh4i8JAYN6pRReKwMiooOElEGYkllUIgVTBhZRGHRKnBbaeGyOf+smshLeO/56xCMvHcJ+ty+l8/h+/3Cf6Q/w4VPYtvrkZk2c/DRzlQmbc3ajiKKnvvpC05MBcIfWb/5PTJ9RKWZRYJoUkRS77Ld+eL8Os/YMOM3z8PEKI1HY0xNFngaH6KuPsKHOShaNljSArUNLVxsuP5bc3zwTfuDpKB2n6CpQ0C12NIYE32js6L9dkK0nOkUL6dtUVW3W1xKpMX+1rOCrbEXADLAhuzA2ubtIfaEVDBtwMmRE8foudFNua5x9VQXd/oHKFNU4re6WOHR4cd0uCRI9vRa8VfDPLx3DZxOmtoO4xSCTRtrUL1eMDIEdRVddaNrOj6tDITDLAmg8DZvLyN66CQ4ZNLjBRLPhviay2MYBlBBld+Dx1OOoigskSQIrMyUStz7WKh3Tzf3OueKu8yaerBMiN+nsno1a1b5eZ7Konl9GO7laEtdVLqVsW8uf2yy82D/ohlb04LL5zquzAfDO8h/XkB2fEd25fAGvqBWpAhFnnBAGvm7T/r3+QVQzKvI28pwFwAAAABJRU5ErkJggg==',
             'wizard': True,
             'options': [
